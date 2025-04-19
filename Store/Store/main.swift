a848be2d05a67c9af4128fc6c1abac950c6ee937
TheJuanCG <114948315+TheJuanCG@users.noreply.github.com>
2025-04-19 00:43:34 -0700

    Finished basic Homework

--- Store/Store/main.swift
+++ Store/Store/main.swift
@@ -7,18 +7,116 @@
 
 import Foundation
 
-protocol SKU {}
+protocol SKU {
+    // name of item and require get
+    var name: String { get }
+    
+    // Gets price in USD Pennies
+    func price() -> Int
+}
 
-class Item {}
+class Item: SKU {
+    private var _name: String
+    
+    var name: String {
+        get {return _name}
+    }
+    
+    var priceEach: Int;
+    
+    init(name: String, priceEach: Int) {
+        self._name = name
+        self.priceEach = priceEach
+    }
+    
+    func price() -> Int { return priceEach }
+    
+}
 
-class Receipt {}
+class Receipt {
+    // What if you have multiple of SKUs?
+    // Can get the item name from skus
+    var itemsList: [Item]
+    
+    init() {
+        self.itemsList = []
+    }
+    
+    // Return list of skus
+    func items() -> [Item] {
+        return itemsList
+    }
+    
+    func add(item: Item) {
+        itemsList.append(item)
+    }
+    
+    func total() -> Int {
+        var sum = 0
+        for item in itemsList {
+            sum += item.price()
+        }
+        
+        return sum
+    }
+    
+    func output() -> String {
+        var res = "Receipt:\n"
+        for item in itemsList {
+            let dollars = item.price() / 100
+            let cents = item.price() % 100
+            let formatted = String(format: "$%d.%02d", dollars, cents)
+            
+            res += "\(item.name): \(formatted)\n"
+        }
+        
+        res += "------------------\n"
+        let totalDollars = total() / 100
+        let cents = total() % 100
+        let formatted = String(format: "$%d.%02d", totalDollars, cents)
+        
+        res += "TOTAL: \(formatted)"
+        return res
+    }
+}
 
-class Register {}
+class Register {
+    var receipt: Receipt
+    
+    init() {
+        self.receipt = Receipt()
+    }
+    
+    func scan(_ item: Item) {
+        receipt.add(item: item);
+    }
+    
+    // TO-DO: Return current total of all items
+    // need to implement receipt first
+    func subtotal() -> Int {
+        var sum = 0
+        for item in receipt.items() {
+            sum += item.price()
+        }
+        
+        return sum
+    }
+    
+    // TO-DO: Return receipt and clears its state
+    func total() -> Receipt {
+        let res = receipt
+        receipt = Receipt()
+        
+        return res
+    }
+
+}
 
 class Store {
     let version = "0.1"
     func helloWorld() -> String {
         return "Hello world"
     }
+    
 }
 
